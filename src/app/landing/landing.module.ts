26daabcc53e4314a0db369993870b2ee423e4b92
Vfranco <victor.franco@cparking.com.co>
2019-10-29 09:22:31 -0500

    SubastaYA WebSite

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SessionModule } from '../session/session.module';
+import { TopbarComponent } from './topbar/topbar.component';
+import { SectionComponent } from './section/section.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
     declarations : [
-        HomeComponent,
-        NavbarComponent        
+        NavbarComponent, TopbarComponent, SectionComponent, FooterComponent, HeaderComponent
     ],
     exports : [
-        HomeComponent,
-        NavbarComponent
+        NavbarComponent, TopbarComponent, SectionComponent, FooterComponent, HeaderComponent
     ],
     imports : [
         CommonModule,
